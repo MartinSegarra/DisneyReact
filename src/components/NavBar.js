65fb65142363c496ac75c2afd7c6d2ae02de8768
Martin Segarra <martinsegarra1994@hotmail.com>
2021-04-13 23:08:11 -0300

    navBar inicial

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../App.css';
+import Logo from './logo.png'
+
+const NavBar = () => {
+    return (
+        <div >
+        <header className="header">
+            <img src={Logo} className="logo"></img>
+             
+         <nav className="navigationBar">
+            <ul className="navigationLinks">
+                 <li className = "navLink"><a href="">Home</a></li>
+                 <li className = "navLink"><a href="">Parques</a></li>
+                 <li className = "navLink"><a href="">Merchandising</a></li>
+                 <li className = "navLink"><a href="">Películas</a></li>
+                 <li className = "navLink"><a href="">Disney+</a></li>
+             </ul>
+         </nav>
+         </header>
+         </div>
+    )
+ }
+
+
+ export default NavBar
